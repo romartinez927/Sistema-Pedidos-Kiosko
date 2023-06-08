7a44b2a13feeb00270bd40075a020d4a511bd901
Rosario Martinez <111587388+romartinez927@users.noreply.github.com>
2023-06-07 21:50:05 -0300

    actualizacion

--- src/components/ImageUploaderContainer/ImageUploaderContainer.jsx
+++ src/components/ImageUploaderContainer/ImageUploaderContainer.jsx
@@ -10,50 +10,50 @@ function ImageUploaderContainer() {
     const [imageUrl, setImageUrl] = useState('');
     const [isLoading, setIsLoading] = useState(false);
   
-    // const handleImageUpload = async () => {
-    //   const formData = new FormData()
-    //   formData.append('image', selectedImage)
+    const handleImageUpload = async () => {
+      const formData = new FormData()
+      formData.append('image', selectedImage)
   
-    //   try {
-    //     const res = await axios.post('http://localhost:4000/upload', formData, {
-    //       headers: {
-    //         'Content-Type': 'multipart/form-data'
-    //       }
-    //     });
+      try {
+        const res = await axios.post('http://localhost:4000/upload', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        });
   
-    //     setImageUrl(res.data.imageUrl)
-    //     setIsLoading(true)
+        setImageUrl(res.data.imageUrl)
+        setIsLoading(true)
 
-    //     setTimeout(() => {
-    //       setIsLoading(false)
-    //     }, 2000)
-    //   } catch (error) {
-    //     console.error(error)
-    //   }
-    // };
+        setTimeout(() => {
+          setIsLoading(false)
+        }, 2000)
+      } catch (error) {
+        console.error(error)
+      }
+    };
   
     // const handleImageChange = (event) => {
     //   setSelectedImage(event.target.files[0])
     // };
 
 
-    const handleImageUpload = async () => {
-      if (selectedImage) {
-        const formData = new FormData();
-        formData.append('file', selectedImage);
-        formData.append('upload_preset', 'images'); // Reemplaza con tu propio upload preset
+    // const handleImageUpload = async () => {
+    //   if (selectedImage) {
+    //     const formData = new FormData();
+    //     formData.append('file', selectedImage);
+    //     formData.append('upload_preset', 'images'); // Reemplaza con tu propio upload preset
   
-        try {
-          const response = await axios.post(
-            'https://api.cloudinary.com/v1_1/dm9aq12zw/image/upload',
-            formData
-          );
-          console.log('URL de imagen subida:', response.data.secure_url);
-        } catch (error) {
-          console.error('Error al subir la imagen:', error);
-        }
-      }
-    };
+    //     try {
+    //       const response = await axios.post(
+    //         'https://api.cloudinary.com/v1_1/dm9aq12zw/image/upload',
+    //         formData
+    //       );
+    //       console.log('URL de imagen subida:', response.data.secure_url);
+    //     } catch (error) {
+    //       console.error('Error al subir la imagen:', error);
+    //     }
+    //   }
+    // };
   
     const handleImageChange = (e) => {
       setSelectedImage(e.target.files[0]);
